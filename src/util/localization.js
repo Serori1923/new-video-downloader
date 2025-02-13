41038dd46505c2c44834e78b3d589dd4e7396799
Serori1926 <test1923001@gmail.com>
2025-02-13 21:43:01 +0800

    add text for Server URL setting

--- src/util/localization.js
+++ src/util/localization.js
@@ -5,6 +5,7 @@ export const localization = {
         languageSetting: {"zh-TW": "語言設置", "en-US": "Language Setting", "ja-JP": "言語設定", "ko-KR": "언어 설정", "tr-TR": "Dil Ayarları"},
         language: {"zh-TW": "語言", "en-US": "Language", "ja-JP": "言語", "ko-KR": "언어", "tr-TR": "Dil"},
         apiServerURL: {"zh-TW": "API 伺服器網址", "en-US": "API Server URL", "ja-JP": "APIサーバーURL", "ko-KR": "API 서버 URL", "tr-TR": "API Sunucu URL"},
+        resetServerURL: { "zh-TW": "重置伺服器網址", "en-US": "Reset Server URL", "ja-JP": "サーバーURLをリセット", "ko-KR": "서버 URL 초기화", "tr-TR": "Sunucu URL'sini Sıfırla"},
         autoDownload: {"zh-TW": "自動下載", "en-US": "Auto Download", "ja-JP": "自動ダウンロード", "ko-KR": "자동 다운로드", "tr-TR": "Otomatik İndirme"},
     },
     downloadButton: {"zh-TW": "下載", "en-US": "Download", "ja-JP": "ダウンロード", "ko-KR": "다운로드", "tr-TR": "İndir"},
