a3c2bc93974b4e190241173477bce118615c935d
Serori1926 <test1923001@gmail.com>
2026-04-27 21:00:46 +0800

    feat: add localization settings for picker page

--- src/util/localization/localizationTwitterPicker.js
+++ src/util/localization/localizationTwitterPicker.js
@@ -1,3 +1,8 @@
 export const localizationTwitterPicker = {
-    rightClickToDownload: {"zh-TW": "點擊右鍵另存影像", "en-US": "Right click to download", "ja-JP": "右クリックしてダウンロード", "ko-KR": "우클릭하여 다운로드", "tr-TR": "Sağa tıklayıp indir"}
+    downloading: {"zh-TW": "下載中...", "en-US": "Downloading...", "ja-JP": "ダウンロード中...", "ko-KR": "다운로드 중...", "tr-TR": "İndiriliyor..."},
+    downloadVideo : {"zh-TW": "下載影片", "en-US": "Download Video", "ja-JP": "動画をダウンロード", "ko-KR": "동영상 다운로드", "tr-TR": "Videoyu İndir"},
+    downloadImage : {"zh-TW": "下載圖片", "en-US": "Download Image", "ja-JP": "画像をダウンロード", "ko-KR": "이미지 다운로드", "tr-TR": "Resmi İndir"},
+    downloadGIF : {"zh-TW": "下載GIF", "en-US": "Download GIF", "ja-JP": "GIFをダウンロード", "ko-KR": "GIF 다운로드", "tr-TR": "GIF'i İndir"},
+    downloadSuccess: {"zh-TW": "下載成功：", "en-US": "Download successful: ", "ja-JP": "ダウンロード成功：", "ko-KR": "다운로드 성공：", "tr-TR": "İndirme başarılı: "},
+    downloadFail: {"zh-TW": "下載失敗，請檢查網路狀態，或檔案已過期，請重新下載", "en-US": "Download failed, please check your network status, or the file has expired, please download again", "ja-JP": "ダウンロードに失敗しました。ネットワークの状態を確認するか、ファイルが期限切れになっている可能性があります。再度ダウンロードしてください", "ko-KR": "다운로드 실패, 네트워크 상태를 확인하거나 파일이 만료되었을 수 있습니다. 다시 다운로드해주세요", "tr-TR": "İndirme başarısız oldu, lütfen ağ durumunuzu kontrol edin veya dosyanın süresi dolmuş olabilir, lütfen tekrar indirin"},
 };
